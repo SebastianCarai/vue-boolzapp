15e2ca0ed7ba3e60fa79d5672f39c786fcb87d11
SebastianCarai <sebastiancarai7@gmail.com>
2021-12-14 15:19:41 +0100

    milestone 5

--- milestone  5/js/script.js
+++ milestone  5/js/script.js
@@ -1,5 +1,5 @@
-// Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i contatti il cui nome contiene le lettere inserite 
-// (es, Marco, Matteo Martina -> Scrivo “mar” rimangono solo Marco e Martina)
+// Cancella messaggio: cliccando sul messaggio appare un menu a tendina che permette di cancellare il messaggio selezionato
+// Visualizzazione ora e ultimo messaggio inviato/ricevuto nella lista dei contatti.
 
 
 Vue.config.devtools = true;
@@ -20,17 +20,20 @@ const app = new Vue(
                         {
                             date: '10/01/2020 15:30:55',
                             text: 'Hai portato a spasso il cane?',
-                            status:'sent'
+                            status:'sent',
+                            dropDownVisible: false,
                         },
                         {
                             date: '10/01/2020 15:50:00',
                             text: 'Ricordati di dargli da mangiare',
-                            status:'sent'
+                            status:'sent',
+                            dropDownVisible: false,
                         },
                         {
                             date: '10/01/2020 16:15:22',
                             text: 'Tutto fatto!',
-                            status:'received'
+                            status:'received',
+                            dropDownVisible: false,
                         }
                     ],
                 },
@@ -42,17 +45,20 @@ const app = new Vue(
                         {
                             date: '20/03/2020 16:30:00',
                             text: 'Ciao come stai?',
-                            status: 'sent'
+                            status: 'sent',
+                            dropDownVisible: false,
                         },
                         {
                             date: '20/03/2020 16:30:55',
                             text: 'Bene grazie! Stasera ci vediamo?',
-                            status: 'received'
+                            status: 'received',
+                            dropDownVisible: false,
                         },
                         {
                             date: '20/03/2020 16:35:00',
                             text: 'Mi piacerebbe ma devo andare a fare la spesa.',
-                            status: 'sent'
+                            status: 'sent',
+                            dropDownVisible: false,
                         }
                     ],
                 },
@@ -64,17 +70,20 @@ const app = new Vue(
                         {
                             date: '28/03/2020 10:10:40',
                             text: 'La Marianna va in campagna',
-                            status: 'received'
+                            status: 'received',
+                            dropDownVisible: false,
                         },
                         {
                             date: '28/03/2020 10:20:10',
                             text: 'Sicuro di non aver sbagliato chat?',
-                            status: 'sent'
+                            status: 'sent',
+                            dropDownVisible: false,
                         },
                         {
                             date: '28/03/2020 16:15:22',
                             text: 'Ah scusa!',
-                            status: 'received'
+                            status: 'received',
+                            dropDownVisible: false,
                         }
                     ],
                 },
@@ -86,12 +95,14 @@ const app = new Vue(
                         {
                             date: '10/01/2020 15:30:55',
                             text: 'Lo sai che ha aperto una nuova pizzeria?',
-                            status: 'sent'
+                            status: 'sent',
+                            dropDownVisible: false,
                         },
                         {
                             date: '10/01/2020 15:50:00',
                             text: 'Si, ma preferirei andare al cinema',
-                            status: 'received'
+                            status: 'received',
+                            dropDownVisible: false,
                         }
                     ],
                 },
@@ -106,7 +117,8 @@ const app = new Vue(
                     this.contacts[this.contactIndex].messages.push({
                         text: 'ok',
                         date: dayjs().format('DD/MM/YYYY HH:mm:ss'),
-                        status: 'received'
+                        status: 'received',
+                        dropDownVisible: false
                     });
                 }, 1000)
             },
@@ -115,13 +127,15 @@ const app = new Vue(
                     this.contacts[this.contactIndex].messages.push({
                         text: this.newTextMessage,
                         date: dayjs().format('DD/MM/YYYY HH:mm:ss'),
-                        status: 'sent'
+                        status: 'sent',
+                        dropDownVisible: false
                     });
                     this.newTextMessage='';
                     this.sendReply();
                 }
             },
             filterContact: function(){
+                // It searchs letters written by the user in the contact's name, and if they are not included, this one displays as none
                 this.contacts.forEach(element => {
                     if (!element.name.toLowerCase().includes(this.contactSearch.toLowerCase())){
                         element.visible = false;
@@ -129,7 +143,21 @@ const app = new Vue(
                         element.visible = true;
                     }
                 });
-                console.log(this.contactSearch)
+            },
+            appearDropdown: function(contactIndex, index){
+
+                // The dropdown menu pops up
+                this.contacts[contactIndex].messages[index].dropDownVisible = ! this.contacts[contactIndex].messages[index].dropDownVisible;
+
+                // If the user clicks another dropdown icon, all the other different dropdown menus will close
+                this.contacts[contactIndex].messages.forEach(element => {
+                    if (Object.is(element, this.contacts[contactIndex].messages[index]) === false && element.dropDownVisible === true){
+                        element.dropDownVisible = false;
+                    }
+                })
+            },
+            deleteMessage: function(contactIndex ,index){
+                this.contacts[contactIndex].messages.splice(index, 1);
             }
         }
     }
